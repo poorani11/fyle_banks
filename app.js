b6b8f9c89f8654c295d6e2fc1ed865905aa2b42f
poorani11 <poorani.mgr@gmail.com>
2016-05-13 12:21:20 +0530

    Addded routing for the page

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-app.js
+// MODULE
+var bankApp = angular.module('bankApp', ['ngRoute', 'ngResource']);
+
+// ROUTES
+bankApp.config(function ($routeProvider){
+
+    $routeProvider
+
+    .when('/', {
+        templateUrl: 'pages/home.html',
+        controller: 'homeController'
+    })
+    .when('/bank', {
+        templateUrl: 'pages/bank.html',
+        controller: 'bankController'
+    })
+});
+
+
+// CONTROLLERS
+bankApp.controller('homeController', ['$scope', function($scope){
+
+}]);
+
+bankApp.controller('bankController', ['$scope', function($scope){
+
+}]);
