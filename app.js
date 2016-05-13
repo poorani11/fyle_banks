2e96645585e9f5aa2422d1328c9833005db446ca
poorani11 <poorani.mgr@gmail.com>
2016-05-13 17:48:00 +0530

    Added 10 cities to the city list

--- app.js
+++ app.js
@@ -22,32 +22,57 @@ bankApp.service('cityService', function(){
     this.city = "BANGALORE";
 
 });
+bankApp.service('ApiService', ['$resource', '$q', function($resource, $q){
+    var vm = this;
+ 
+    vm.getApi = function(_city) {
+
+        return $q(function(resolve, reject) {
+
+            var resObj = $resource("https://api.fyle.in/api/bank_branches");
+            var resp = resObj.query({city:_city,offset: 0,limit:50});
+            resp.$promise.then(function(data){
+                   console.log('Got data');
+                   vm.data = data;
+                   console.log(data);
+                   resolve('success')
+                 },function(err){
+                    console.log(err);
+                    console.log('error');
+                    reject('bjfshgfkj')
+            });
+        })
+    // console.log($scope.bankResult);
+
+    }
+    vm.getBankData = function(){
+        return vm.data;
+    }
+}]);
 
 
 // CONTROLLERS
-bankApp.controller('homeController', ['$scope','cityService', function($scope,cityService){
+bankApp.controller('homeController', ['$scope','cityService','ApiService', function($scope,cityService,ApiService){
     $scope.city = cityService.city;
     $scope.$watch('city', function(){
         cityService.city = $scope.city;
 
     });
-
+   
+    $scope.selectCity = ['BANGALORE','DELHI', 'MUMBAI','KOLKATA', 'CHANDIGARH','LUCKNOW','HYDERABAD','COCHIN','PUNE','JAIPUR'];
+     ApiService.getApi($scope.city)
+    .then(function(result) {
+        $scope.bankData = ApiService.getBankData();
+        
+    })
 }]);
 
-bankApp.controller('bankController', ['$scope','$resource','cityService', function($scope,$resource,cityService){
+bankApp.controller('bankController', ['$scope','$resource','cityService','ApiService', function($scope,$resource,cityService,ApiService){
     $scope.city = cityService.city;
-        $scope.bankAPI = $resource("https://api.fyle.in/api/bank_branches");
-    $scope.bankResult = $scope.bankAPI.query({city:$scope.city,offset: 0,limit:50});
-    $scope.bankResult.$promise.then(function(data){
-        console.log('Got data');
-        console.log(data);
-    },function(err){
-        console.log(err);
-        console.log('error');
-    });
-    console.log($scope.bankResult);
-
-    
-
+   ApiService.getApi($scope.city)
+    .then(function(result) {
+        $scope.bankData = ApiService.getBankData();
+        
+    })
 
 }]);
